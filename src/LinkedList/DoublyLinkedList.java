7a5182e174bd8f5cc854c2491ba47a9def0b88c3
justinabhi1 <saiabhinavsikhakolli@gmail.com>
2025-01-19 15:28:14 +0530

    DLL get Commit

--- src/LinkedList/DoublyLinkedList.java
+++ src/LinkedList/DoublyLinkedList.java
@@ -95,4 +95,20 @@ public class DoublyLinkedList {
         length--;
         return temp;
     }
+
+    public Node get(int index) {
+        if( index < 0 || index >= length) return null;
+        Node temp = head;
+        if (length/2 < index) {
+            for (int i=0; i<index;i++) {
+                temp = temp.next;
+            }
+        }else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
 }
