22f6c51e27bc5a5354eb2a9ac132ad38f44e4828
justinabhi1 <saiabhinavsikhakolli@gmail.com>
2026-03-29 12:09:12 +0530

    rename field

--- src/Arrays_and_Hashing/subarraySumEqualsK.java
+++ src/Arrays_and_Hashing/subarraySumEqualsK.java
@@ -6,18 +6,18 @@ import java.util.Map;
 public class subarraySumEqualsK {
     public static int  subarraySumEqualsK(int[] nums, int k) {
         // to track the how many subarrays that got prefixes when subtracting with k
-        Map<Integer, Integer> sumCountMap = new HashMap<>();
-        sumCountMap.put(0,1);
+        Map<Integer, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(0,1);
 
         int result =0;
         int prefix = 0;
         for (int num : nums) {
             prefix+= num;
-            if (sumCountMap.containsKey(prefix - k)) {
-                result+= sumCountMap.get(prefix - k);
+            if (prefixSum.containsKey(prefix - k)) {
+                result+= prefixSum.get(prefix - k);
             }
 
-            sumCountMap.put(prefix, sumCountMap.getOrDefault(prefix, 0) + 1);
+            prefixSum.put(prefix, prefixSum.getOrDefault(prefix, 0) + 1);
         }
         return result;
 
