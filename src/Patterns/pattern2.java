9ab2323f251e7ac41319fb7d083edb0b85e74c02
justinabhi1 <saiabhinavsikhakolli@gmail.com>
2026-04-06 18:31:49 +0530

    pattern2.java

---
+++ src/Patterns/pattern2.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        // right-angled triangle
+
+        Scanner sc = new Scanner(System.in);
+        Integer number = sc.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
